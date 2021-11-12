282f38582de289dbff520749cb5f846e4a5985a6
root <root@DESKTOP-S1BDJ1O.localdomain>
2021-11-12 18:17:49 +0100

    deploy

--- studybud/settings.py
+++ studybud/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+import django_heroku
+
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,13 +128,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATIC_ROOT =   os.path.join(BASE_DIR, 'staticfiles')
+
 
 STATIC_URL = "/static/"
 
 # tope added
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = os.path.join(BASE_DIR,  "static")
 MEDIA_URL = '/images/'
-MEDIA_ROOT = BASE_DIR / 'static/images'
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'static/images')
+django_heroku.settings(locals())
 
 
 
