15666bf1cbb255fa622b3500c152b83e45c4e580
Blazskills <ilesanmiisaac@gmail.com>
2021-11-14 01:02:26 +0100

    media url fixes

--- base/models.py
+++ base/models.py
@@ -8,8 +8,7 @@ class User(AbstractUser):
     name = models.CharField(max_length=200, null=True)
     email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True)
-
-    avatar = models.ImageField(null=True, default="avatar.svg")
+    avatar = models.ImageField(null=True, upload_to='images', default="avatar.svg")
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
